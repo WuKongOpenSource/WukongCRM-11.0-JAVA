d171ee2b5d74a40bfc5e459f11fbe3a43db377a2
banma <banma@5kcrm.com>
2020-10-24 18:54:00 +0800

    提示语优化

--- docker/start.sh
+++ docker/start.sh
@@ -3,4 +3,4 @@ chmod 777 nacos.sh;
 chmod 777 wkcrm.sh;
 chmod 777 data/elasticsearch/data/
 docker-compose up -d
-echo " ------------ 请等待十分钟后访问系统... ------------ ";
+echo " 系统正在启动，启动过程大约需要10分钟，请10分钟后访问系统。 ";
